df32207191cf1a59eb69f2e9fae1acf0361774eb
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-04-17 10:55:42 +0100

    Made new script to translate sequences and reference into aminoacids and then back translated with a reduced aminoacid table

---
+++ retranslate.py
@@ -0,0 +1,32 @@
+
+
+# NCBI standard codon table (acquired http://www.bioinformatics.org/JaMBW/2/3/TranslationTables.html)
+codon_table = {
+    'A': ('GCT', 'GCC', 'GCA', 'GCG'),
+    'C': ('TGT', 'TGC'),
+    'D': ('GAT', 'GAC'),
+    'E': ('GAA', 'GAG'),
+    'F': ('TTT', 'TTC'),
+    'G': ('GGT', 'GGC', 'GGA', 'GGG'),
+    'I': ('ATT', 'ATC', 'ATA'),
+    'H': ('CAT', 'CAC'),
+    'K': ('AAA', 'AAG'),
+    'L': ('TTA', 'TTG', 'CTT', 'CTC', 'CTA', 'CTG'),
+    'M': ('ATG',),
+    'N': ('AAT', 'AAC'),
+    'P': ('CCT', 'CCC', 'CCA', 'CCG'),
+    'Q': ('CAA', 'CAG'),
+    'R': ('CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'),
+    'S': ('TCT', 'TCC', 'TCA', 'TCG', 'AGT', 'AGC'),
+    'T': ('ACT', 'ACC', 'ACA', 'ACG'),
+    'V': ('GTT', 'GTC', 'GTA', 'GTG'),
+    'W': ('TGG',),
+    'Y': ('TAT', 'TAC'),
+    '*': ('TAA', 'TAG', 'TGA'),
+}
+
+# Take the first item for each key in the codon_table dictionary to produce a reduced codon library
+reduced_codon = {}
+
+for keys in codon_table.keys():
+    reduced_codon[keys] = codon_table[keys][0]
