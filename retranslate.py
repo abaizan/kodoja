375dec9a8ccc695f495d23fddac7dfa283996aff
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-05-24 11:40:02 +0100

    Added function to take in translated data and retranslate it using the reduced codon table and output the data

--- retranslate.py
+++ retranslate.py
@@ -1,4 +1,4 @@
-
+# Python 2.7
 
 # NCBI standard codon table (acquired http://www.bioinformatics.org/JaMBW/2/3/TranslationTables.html)
 codon_table = {
@@ -30,3 +30,41 @@ reduced_codon = {}
 
 for keys in codon_table.keys():
     reduced_codon[keys] = codon_table[keys][0]
+
+
+# Replace each aa with codon from reduced_codon. Transeq adds letters (eg. 'B' and 'X' for unknown codons) which will be back translated to 'N' as kraken doesn't identify them
+
+def retranslate(protein_file, outfile_name):
+    with open(protein_file, 'r') as f:
+        protein_list = f.read().splitlines()
+        protein_list = filter(None,  protein_list)
+    for line in range(0,len(protein_list)):
+        if protein_list[line][0] == '>':
+            continue
+        list_line = list(protein_list[line])
+        list_line = [reduced_codon.get(aa, 'N') for aa in list_line]
+        protein_list[line] = ''.join(list_line)
+
+    outfile = open(outfile_name, 'w')
+    for item in protein_list:
+        outfile.write("%s\n" % item)
+
+
+# Back translate the reference sequence (viral)
+#retranslate(protein_file = "/home/ae42909/Scratch/emboss_transeq/twolines_concatinatedRefseqViral_genomic.tax.fna",
+#            outfile_name = '/home/ae42909/Scratch/emboss_transeq/retranslated_RefSeqViruses.fna')
+
+# Back translate the fastq files
+retranslate(protein_file = "PE1_subset",
+            outfile_name = 'PE1_retranslated')
+
+retranslate(protein_file = "PE2_subset",
+            outfile_name = 'PE2_retranslated')
+
+
+
+
+
+
+
+
