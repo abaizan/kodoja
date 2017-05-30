66c4427b6bb04b93d3f06fd6ffe0032a20e485c4
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-05-30 14:52:00 +0100

    Added command to stop script if error occurs in subscript

--- diagnostic_master.sh
+++ diagnostic_master.sh
@@ -3,37 +3,40 @@
 #$ -cwd
 #$ -j yes
 #$ -pe smp 4
-export THREADS=4
+set -e
 
+export THREADS=4
 export FILE1=/home/ae42909/synthetic_RNAseq/mappingRNAseq/concatenated_fastaFiles/Potato_withViruses_1.fastq
 export FILE2=/home/ae42909/synthetic_RNAseq/mappingRNAseq/concatenated_fastaFiles/Potato_withViruses_2.fastq
 export FORMAT=fastq
-export OUTDIR=/home/ae42909/Scratch/synthPotato_pipeline
+export OUTDIR=/home/ae42909/Scratch/synthPotato_pipeline/
 export TRIMLEN=50
 
+# Test data format
+python /home/ae42909/scripts_inProcess/testing_format.py
 
 # QC and trim data
-#/home/ae42909/scripts_inProcess/fastqc.sh $TRIMLEN $FILE1 $FILE2
+/home/ae42909/scripts_inProcess/fastqc.sh $TRIMLEN $FILE1 $FILE2
 
 # Order and replace names
 /home/ae42909/scripts_inProcess/rename_sequences.sh
 
 # Kraken nucleotide analysis
-#/home/ae42909/scripts_inProcess/kraken_nucleotides.sh
+/home/ae42909/scripts_inProcess/kraken_nucleotides.sh
 
 # Pull out sequences for re-analysis
-#python /home/ae42909/scripts_inProcess/sequences_reanalysis.py
+python /home/ae42909/scripts_inProcess/sequences_reanalysis.py
 
 # Kaiju classification
-#/home/ae42909/scripts_inProcess/kaiju_nucleotide.sh
+/home/ae42909/scripts_inProcess/kaiju_nucleotide.sh
 
 # Emboss translation
-#/home/ae42909/scripts_inProcess/translate_nucleotide.sh
+/home/ae42909/scripts_inProcess/translate_nucleotide.sh
 
 # Backtranslate
-#python /home/ae42909/scripts_inProcess/backtranslate.py
+python /home/ae42909/scripts_inProcess/backtranslate.py
 
 # Kraken analysis of backtranslate
-#/home/ae42909/scripts_inProcess/kraken_aminoacids.sh
+/home/ae42909/scripts_inProcess/kraken_aminoacids.sh
 
 # Result analysis
