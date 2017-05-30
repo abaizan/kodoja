0e5d218d27bb6ec285107e59c2a5dc275c309371
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-05-30 15:08:58 +0100

    Removed a space so that code could work and added an intermediate file

--- translate_nucleotide.sh
+++ translate_nucleotide.sh
@@ -3,9 +3,9 @@
 sed 's/\/1$//' PE1_subset > PE1_short # remove the '/1' to be added later for kraken analysis, as kraken recognises "/1" as a paired read
 sed 's/\/2$//' PE2_subset > PE2_short
 
-transeq  PE1_short PE1_translated -frame=6
-transeq  PE2_short PE2_translated -frame=6
+transeq PE1_short PE1_trans -frame=6
+transeq PE2_short PE2_trans -frame=6
 
 
-cat PE1_translated |  awk '{print (NR%2 == 1) ? $0 "/1"  :$0}' > PE1_translated
-cat PE2_translated |  awk '{print (NR%2 == 1) ? $0 "/2"  :$0}' > PE2_translated
+cat PE1_trans |  awk '{print (NR%2 == 1) ? $0 "/1"  :$0}' > PE1_translated
+cat PE2_trans |  awk '{print (NR%2 == 1) ? $0 "/2"  :$0}' > PE2_translated
