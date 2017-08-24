49b2ad06f1d4eed06a9571694f497b19af8423fa
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-08-24 09:20:18 +0100

    Added file paths to test fasta files and modified the code so that it only tests if there is a second file (ie paired end) once

--- diagnosticTool_scripts/diagnostic_master.py
+++ diagnosticTool_scripts/diagnostic_master.py
@@ -1,15 +1,19 @@
 import os
 import time
-from diagnostic_module import *
+from diagnostic_modules import *
 
+file2 = False
 threads = "4"
-file1 = "/home/ae42909/synthetic_RNAseq/mappingRNAseq/concatenated_fastaFiles/Potato_withViruses_1.fastq"
-file2 = "/home/ae42909/synthetic_RNAseq/mappingRNAseq/concatenated_fastaFiles/Potato_withViruses_2.fastq"
+# file1 = "/home/ae42909/synthetic_RNAseq/mappingRNAseq/concatenated_fastaFiles/Potato_withViruses_1.fastq"
+# file2 = "/home/ae42909/synthetic_RNAseq/mappingRNAseq/concatenated_fastaFiles/Potato_withViruses_2.fastq"
 #file1 = "/home/ae42909/Scratch/100_Potato_withViruses_1.fastq"
 #file2 = "/home/ae42909/Scratch/100_Potato_withViruses_2.fastq"
-user_format = "fastq"
-out_dir = "/home/ae42909/Scratch/synthPotato_krakenDB_viral"
-#out_dir = "/home/ae42909/Scratch/100Seq_krakenDB_viral"
+file1 = "/home/ae42909/Scratch/100_Potato_withViruses_1.fasta"
+#file2 = "/home/ae42909/Scratch/100_Potato_withViruses_2.fasta"
+user_format = "fasta"
+#user_format = "fastq"
+# out_dir = "/home/ae42909/Scratch/synthPotato_krakenDB_viral"
+out_dir = "/home/ae42909/Scratch/100Seq_krakenDB_viral"
 trimlen = "50"
 kraken_db = "/home/ae42909/Scratch/krakenDB_viral/"
 kaiju_nodes = "/mnt/shared/scratch/ae42909/201609_BBSRC_Diagnostics/kaiju/kaijudb/nodes.dmp"
@@ -28,40 +32,72 @@ if not os.path.exists(out_dir):
 os.chdir(out_dir)
 
 t0 = time.time()
+
 # Test data format - fasta or fastq
 test_format(file1, user_format)
 t1 = time.time()
 
-# QC and trim data
-fastqc_trim(out_dir, file1, trimlen, threads, file2)
-t2 = time.time()
 
-# Order and replace names
 if file2:
-    rename_seq("PE_trimmed_data_1P", out_dir, user_format, paired = "1")
-    rename_seq("PE_trimmed_data_2P", out_dir, user_format, paired = "2")
+    # fasta files cannot be QC'd - only for fastq files
+    if user_format == "fastq":
+        # QC and trim data
+        fastqc_trim(out_dir, file1, trimlen, threads, file2)
+        t2 = time.time()
+        # Order and replace names from trimmed sequences
+        rename_seq("PE_trimmed_data_1P", out_dir, user_format, paired = "1")
+        rename_seq("PE_trimmed_data_2P", out_dir, user_format, paired = "2")
+        t3 = time.time()
+        
+    elif user_format == "fasta":
+        rename_seq(file1, out_dir, user_format, paired = "1")
+        rename_seq(file2, out_dir, user_format, paired = "2")
+
+    # Kraken classification
+    kraken_classify("renamed_file1.", threads, user_format, kraken_db, "renamed_file2.")
+    t4 = time.time()
+
+    # Subset viral and unclassified sequences
+    seq_reanalysis("kraken_table.txt", "kraken_labels.txt", out_dir, user_format, "renamed_file1.", "renamed_file2.")
+    t5 = time.time()
+
+    # Kaiju classification of subset sequences
+    kaiju_classify("subset_file1.", user_format, threads, kaiju_nodes, kaiju_fmi, kaiju_names, kaiju_missmatch, kraken_db, "subset_file2.")
+    t6 = time.time()     
+
+    # Merege results
+    result_analysis(out_dir, "kraken_FormattedTable.txt", "kaiju_table.txt", "kaiju_labels.txt", "ID1.txt", "ID2.txt")
+    t7 = time.time()
 else:
-    rename_seq("SE_trimmed_data", out_dir, user_format, paired = False)
-t3 = time.time()
+    # fasta files cannot be QC'd - only for fastq files
+    if user_format == "fastq":
+        # QC and trim data
+        fastqc_trim(out_dir, file1, trimlen, threads)
+        t2 = time.time()
 
-# Kraken classification
-if file2:
-    kraken_class("renamed_file1.fastq", threads, user_format, kraken_db, "renamed_file2.fastq")
-else:
-    kraken_class("rename_file.fastq", threads, kraken_db, user_format)
-t4 = time.time()
+        # Order and replace names from trimmed sequences
+        rename_seq("SE_trimmed_data", out_dir, user_format, paired = False)
+        t3 = time.time()
+        
+    elif user_format == "fasta":
+        rename_seq(file1, out_dir, user_format, paired = False)
+        t3 = time.time()
 
-# Subset viral and unclassified sequences
-seq_reanalysis("kraken_table.txt", "kraken_labels.txt", out_dir, user_format, "renamed_file1.fastq", "renamed_file2.fastq")
-t5 = time.time()
+    # Kraken classification
+    kraken_classify("renamed_file.", threads, user_format, kraken_db,)
+    t4 = time.time()
 
-# Kaiju classification of subset sequences
-kaiju_class("subset_file1.fastq", threads, kaiju_nodes, kaiju_fmi, kaiju_names, kaiju_missmatch, kraken_db, "subset_file2.fastq")
-t6 = time.time()
+    # Subset viral and unclassified sequences
+    seq_reanalysis("kraken_table.txt", "kraken_labels.txt", out_dir, user_format, "renamed_file.")
+    t5 = time.time()
 
-# Merege results
-result_analysis(out_dir, "kraken_FormattedTable.txt", "kaiju_table.txt", "kaiju_labels.txt", "ID1.txt", "ID2.txt")
-t7 = time.time()
+    # Kaiju classification of subset sequences
+    kaiju_classify("subset_file1.", user_format, threads, kaiju_nodes, kaiju_fmi, kaiju_names, kaiju_missmatch, kraken_db)
+    t6 = time.time()
+
+    # Merege results
+    result_analysis(out_dir, "kraken_FormattedTable.txt", "kaiju_table.txt", "kaiju_labels.txt", "ID.txt")
+    t7 = time.time()
 
 print "testing format = " + str((t1-t0)/3600)
 print "fastq and trim = " + str((t2-t1)/3600)
