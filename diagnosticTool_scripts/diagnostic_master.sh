9b685e5eb563f2967a49d3e6f71f7f965f04f060
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-08-24 09:21:49 +0100

    Added the new path (after making new subdirectories for script files)

--- diagnosticTool_scripts/diagnostic_master.sh
+++ diagnosticTool_scripts/diagnostic_master.sh
@@ -4,6 +4,5 @@
 #$ -j yes
 #$ -pe smp 4
 export PATH=/home/ae42909/Programs/Kraken/kraken-0.10.5-beta/builddir:/mnt/apps/jellyfish/1.1.11/bin:$PATH
-export PERL5LIB=/mnt/apps/BioPerl-1.6.901
 
-python /home/ae42909/scripts_inProcess/diagnostic_master.py
+python /home/ae42909/scripts/diagnosticTool_scripts/diagnostic_master.py
