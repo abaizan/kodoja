0c3a0d3443e544dd8961cd252285aa56e61f6a3b
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-08-28 11:23:27 +0100

    Added kaiju bin/ file to path rather than being in the script

--- diagnosticTool_scripts/database_master.sh
+++ diagnosticTool_scripts/database_master.sh
@@ -2,7 +2,7 @@
 
 #$ -cwd
 #$ -j yes
-#$ -pe smp 10
-export PATH=/home/ae42909/Programs/Kraken/kraken-0.10.5-beta/builddir:/mnt/apps/jellyfish/1.1.11/bin:$PATH
+#$ -pe smp 1
+export PATH=/home/ae42909/Programs/Kraken/kraken-0.10.5-beta/builddir:/mnt/apps/jellyfish/1.1.11/bin:/home/ae42909/Programs/Kaiju/kaiju-v1.5.0-linux-x86_64-static/bin:$PATH
 
 python /home/ae42909/scripts/diagnosticTool_scripts/database_master.py
