b12ebb92ebcb9c31bb8dda36303aa26cf28d53ec
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-09-05 14:39:15 +0100

    Added kaiju_db path rather than individual files, added function to check all paths have '/' at the end

--- diagnosticTool_scripts/diagnostic_master.py
+++ diagnosticTool_scripts/diagnostic_master.py
@@ -3,31 +3,38 @@ import time
 from diagnostic_modules import *
 
 # General parameters
-file2 = False
 # file1 = "/home/ae42909/synthetic_RNAseq/mappingRNAseq/concatenated_fastaFiles/Potato_withViruses_1.fastq"
 # file2 = "/home/ae42909/synthetic_RNAseq/mappingRNAseq/concatenated_fastaFiles/Potato_withViruses_2.fastq"
-#file1 = "/home/ae42909/Scratch/100_Potato_withViruses_1.fastq"
-#file2 = "/home/ae42909/Scratch/100_Potato_withViruses_2.fastq"
-file1 = "/home/ae42909/Scratch/100_Potato_withViruses_1.fasta"
-#file2 = "/home/ae42909/Scratch/100_Potato_withViruses_2.fasta"
-user_format = "fasta"
+# file1 = "/home/ae42909/Scratch/100_Potato_withViruses_1.fastq"
+# file2 = "/home/ae42909/Scratch/100_Potato_withViruses_2.fastq"
+# file1 = "/home/ae42909/Scratch/100_Potato_withViruses_1.fasta"
+# file2 = "/home/ae42909/Scratch/100_Potato_withViruses_2.fasta"
+file1 = "/mnt/shared/projects/virology/201702_Stuart_local_raspvirus/static/Dee-B5_S1_R1.fastq"
+file2 = "/mnt/shared/projects/virology/201702_Stuart_local_raspvirus/static/Dee-B5_S1_R2.fastq"
+user_format = "fastq"
 # out_dir = "/home/ae42909/Scratch/synthPotato_krakenDB_viral"
-out_dir = "/home/ae42909/Scratch/100Seq_krakenDB_viral"
-threads = "4"
+out_dir = "/home/ae42909/Scratch/berry/Dee-B5_S1"
+user_format = "fastq"
+threads = 4
 # Trimmomatic paramters
-trim_minlen = "50"
+trim_minlen = 50
 adapter_file = "/mnt/apps/trimmomatic/IlluminaAdapters.fasta"
 # Kraken parameters
 kraken_db = "/home/ae42909/Scratch/krakenDB_viral/"
+# quick_minhits = 2
 # Kaiju parameters
-kaiju_nodes = "/mnt/shared/scratch/ae42909/201609_BBSRC_Diagnostics/kaiju/kaijudb/nodes.dmp"
-kaiju_names = "/mnt/shared/scratch/ae42909/201609_BBSRC_Diagnostics/kaiju/kaijudb/names.dmp"
-kaiju_fmi = "/mnt/shared/scratch/ae42909/201609_BBSRC_Diagnostics/kaiju/kaijudb/kaiju_db.fmi"
-kaiju_missmatch = "5"
+kaiju_db = "/home/ae42909/Scratch/kaijuDB_viral/"
+# kaiju_nodes = "/mnt/shared/scratch/ae42909/201609_BBSRC_Diagnostics/kaiju/kaijudb/nodes.dmp"
+# kaiju_names = "/mnt/shared/scratch/ae42909/201609_BBSRC_Diagnostics/kaiju/kaijudb/names.dmp"
+# kaiju_fmi = "/mnt/shared/scratch/ae42909/201609_BBSRC_Diagnostics/kaiju/kaijudb/kaiju_db.fmi"
+kaiju_minlen = 11
+kaiju_score = 65
+kaiju_mismatch = 5
 
-# Check that out_dir has "/" at the end
-if out_dir[-1] != "/":
-    out_dir = out_dir + "/"
+# Check that dirs have "/" at the end
+out_dir += check_path(out_dir)
+kraken_db += check_path(kraken_db)
+kaiju_db += check_path(kaiju_db)
 
 # Check out_dir exits else make dir
 if not os.path.exists(out_dir):
@@ -66,7 +73,7 @@ if file2:
     t5 = time.time()
 
     # Kaiju classification of subset sequences
-    kaiju_classify("subset_file1.", user_format, threads, kaiju_nodes, kaiju_fmi, kaiju_names, kaiju_missmatch, kraken_db, "subset_file2.")
+    kaiju_classify("subset_file1.", user_format, threads, kaiju_db, kaiju_mismatch, kraken_db, "subset_file2.")
     t6 = time.time()     
 
     # Merege results
@@ -96,7 +103,7 @@ else:
     t5 = time.time()
 
     # Kaiju classification of subset sequences
-    kaiju_classify("subset_file1.", user_format, threads, kaiju_nodes, kaiju_fmi, kaiju_names, kaiju_missmatch, kraken_db)
+    kaiju_classify("subset_file1.", user_format, threads, kaiju_db, kaiju_mismatch, kraken_db)
     t6 = time.time()
 
     # Merege results
@@ -111,4 +118,3 @@ print "subset vrl and unclas sequences = " + str((t5-t4)/3600)
 print "kaiju classification = " + str((t6-t5)/3600)
 print "Results = " + str((t7-t6)/3600)
 print "total = " + str((t7-t0)/3600)
-
