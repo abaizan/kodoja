bd18702c9ce50147f410cae7d4793af47e29f23c
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-09-07 11:41:16 +0100

    This repository will now focus on the diagnosis tool only.

--- kaiju_scripts/kaiju_nucleotide.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-KAIJU_nodes=/mnt/shared/scratch/ae42909/201609_BBSRC_Diagnostics/kaiju/kaijudb/nodes.dmp
-KAIJU_fmi=/mnt/shared/scratch/ae42909/201609_BBSRC_Diagnostics/kaiju/kaijudb/kaiju_db.fmi
-OUTPUTNAME=kaiju_ouput
-
-#cd /mnt/shared/scratch/ae42909/201609_BBSRC_Diagnostics/kaiju
-
-/home/ae42909/Programs/Kaiju/kaiju-v1.5.0-linux-x86_64-static/bin/kaiju -z $THREADS -t $KAIJU_nodes -f $KAIJU_fmi -i PE1_subset and -j PE2_subset -o $OUTPUTNAME -x -v -a greedy -e 5
--- kraken_scripts/ftp_download/run_kraken_ftp_viral.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#$ -cwd
-#$ -j yes
-export PERL5LIB=/mnt/apps/BioPerl-1.6.901
-perl /home/ae42909/finished_scripts/kraken_ftp_viral.pl
