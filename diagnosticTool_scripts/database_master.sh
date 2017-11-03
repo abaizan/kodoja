1e6686ec1c301259f5a6bd1b7323e9336478529e
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-11-03 10:57:02 +0000

    Changed the path to the .py file once all scripts were moved to new directory

--- diagnosticTool_scripts/database_master.sh
+++ diagnosticTool_scripts/database_master.sh
@@ -5,4 +5,4 @@
 #$ -pe smp 4
 export PATH=/home/ae42909/Programs/Kraken/kraken-0.10.5-beta/builddir:/mnt/apps/jellyfish/1.1.11/bin:/home/ae42909/Programs/Kaiju/kaiju-v1.5.0-linux-x86_64-static/bin:$PATH
 
-python /home/ae42909/scripts/diagnosticTool_scripts/database_master.py
+python /home/ae42909/viral_diagnostics/diagnosticTool_scripts/database_master.py
--- diagnosticTool_scripts/diagnostic_master.sh
+++ diagnosticTool_scripts/diagnostic_master.sh
@@ -6,4 +6,4 @@
 
 export PATH=/home/ae42909/Programs/Kraken/kraken-0.10.5-beta/builddir:/mnt/apps/jellyfish/1.1.11/bin:/home/ae42909/Programs/Kaiju/kaiju-v1.5.0-linux-x86_64-static/bin:/mnt/apps/trimmomatic/0.32:$PATH
 
-python /home/ae42909/scripts/diagnosticTool_scripts/diagnostic_master.py
+python /home/ae42909/viral_diagnostics/diagnosticTool_scripts/diagnostic_master.py
