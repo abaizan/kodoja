c6c1c3595103b22e84a277587f99698f8f9b2e38
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-11-21 19:05:14 +0000

    Changed parameters for run

--- parameter_testing/kaiju_parameterValues.py
+++ parameter_testing/kaiju_parameterValues.py
@@ -1,8 +1,8 @@
-# run 9:35
-# kaiju_parameters = {"kaiju_minlen":[3, 7, 11, 15],"kaiju_mismatch":[False, 1, 3, 5], "kaiju_score":[45, 65, 85]}
+# run 9:39
+kaiju_parameters = {"kaiju_minlen":[3, 7, 11, 15],"kaiju_mismatch":[False, 1, 3, 5], "kaiju_score":[45, 65, 85]}
 
 # run 3:8 & 40:55
-kaiju_parameters = {"kaiju_minlen":[2, 3, 4],"kaiju_mismatch":[False, 1], "kaiju_score":[45, 65, 85]}
+# kaiju_parameters = {"kaiju_minlen":[2, 3, 4],"kaiju_mismatch":[False, 1], "kaiju_score":[45, 65, 85]}
 
 # Run 1 & 2
 # kaiju_parameters = {"kaiju_minlen":[7, 11, 12, 14],"kaiju_mismatch":[False, 1, 3, 5], "kaiju_score":[45, 65, 85]}
