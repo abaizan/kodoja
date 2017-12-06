15020a5c937a24dfabedeaf0fccaa3356b02d686
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-12-06 09:21:22 +0000

    Added argparse and fixed replacement of ids2

--- diagnosticTool_scripts/diagnostic_seqRetrive.py
+++ diagnosticTool_scripts/diagnostic_seqRetrive.py
@@ -1,39 +1,51 @@
 import os
 import sys
-sys.path.insert(0, '/home/ae42909/viral_diagnostics/diagnosticTool_scripts/')
-from diagnostic_modules import *
+import pandas as pd
+from diagnostic_modules import sequence_subset
+import argparse
+import pickle
 
-file1 = "/home/ae42909/Scratch/smallTest_data/100_Potato_withViruses_1.fastq"
-file2 = False
-user_format = "fastq"
-out_dir = "/home/ae42909/Scratch/100Seq_krakenDB_viral/"
-userList_TaxID = False
+parser = argparse.ArgumentParser(description='Retrieve viral sequences of interest')
+parser.add_argument('-r1', '--read1', type=str, required=True,
+                    metavar='', help='Read 1 file path, required')
+parser.add_argument('-o', '--out_dir', type=str, required=True,
+                    metavar='', help='Output directory path, required')
+parser.add_argument('-r2', '--read2', type=str, default=False,
+                    metavar='', help='Read 2 file path, default: False')
+parser.add_argument('-f', '--user_format', type=str, default='fastq',
+                    metavar='', help='Sequence data format, default: fastq')
+parser.add_argument('-t', '--taxID', type=int, default=False,
+                    metavar='', help='Virus tax ID for subsetting, default: All viral sequences')
+args = parser.parse_args()
 
-table_summary = pd.read_csv(out_dir + "virus_table.txt", sep="\t", header = 0,
+table_summary = pd.read_csv(args.out_dir + "virus_table.txt", sep="\t", header = 0,
                        index_col=False)
-kodoja_vrl = pd.read_csv(out_dir + "kodoja_VRL.txt", sep="\t", header = 0,
+kodoja_vrl = pd.read_csv(args.out_dir + "kodoja_VRL.txt", sep="\t", header = 0,
                        index_col=False)
 
-if userList_TaxID:
-    TaxId_out = userList_TaxID
+if args.taxID:
+    TaxId_out = [args.taxID]
+    label = 'virus_' + str(args.taxID)
     
 else:
     TaxId_out = list(table_summary.Tax_ID)
+    label = 'virus_all'
 
 rows_wanted = (kodoja_vrl['kraken_tax_ID'].isin(TaxId_out) |
                kodoja_vrl['kaiju_tax_ID'].isin(TaxId_out))
 seqID_wanted = list(kodoja_vrl.loc[rows_wanted, 'Seq_ID'])
 
-sequence_subset(out_dir, file1, "viral_sequences1.", user_format,
-                seqID_wanted, 'viral_sequences1.txt')
-if file2:
-    with open(out_dir + 'ids1.pkl', 'rb') as id_dict:
+sequence_subset(args.out_dir, args.read1, label + "_sequences1.", args.user_format,
+                seqID_wanted, label + '_sequences1.txt')
+if args.read2:
+    print 'We got to importing dict'
+    with open(args.out_dir + 'ids1.pkl', 'rb') as id_dict:
         ids1 = pickle.load(id_dict)
-    with open(out_dir + 'ids2.pkl', 'rb') as id_dict:
+    with open(args.out_dir + 'ids2.pkl', 'rb') as id_dict:
         ids2 = pickle.load(id_dict)
     iv_ids1 = dict((v,k) for k,v in ids1.iteritems())
-    kodoja_vrl = kodoja_vrl.replace({"Seq_ID": iv_ids1})
-    kodoja_vrl = kodoja_vrl.replace({"Seq_ID": ids2})
+    kodoja_vrl["Seq_ID"] = kodoja_vrl["Seq_ID"].map(iv_ids1)
+    kodoja_vrl["Seq_ID"] = kodoja_vrl["Seq_ID"].map(ids2)
     seqID_wanted = list(kodoja_vrl.loc[rows_wanted, 'Seq_ID'])
-    sequence_subset(out_dir, file2, "viral_sequences2.", user_format,
-                    seqID_wanted, 'viral_sequences2.txt')
+    sequence_subset(args.out_dir, args.read2, label + "_sequences2.", args.user_format,
+                    seqID_wanted, label + '_sequences2.txt')
--- diagnosticTool_scripts/diagnostic_seqRetrive.sh
+++ diagnosticTool_scripts/diagnostic_seqRetrive.sh
@@ -4,4 +4,4 @@
 #$ -j yes
 #$ -pe smp 1
 
-python /home/ae42909/viral_diagnostics/diagnosticTool_scripts/diagnostic_seqRetrive.py
+python /home/ae42909/viral_diagnostics/diagnosticTool_scripts/diagnostic_seqRetrive.py -r1 '/home/ae42909/Scratch/smallTest_data/100_Potato_withViruses_1.fastq' -o '/home/ae42909/Scratch/100Seq_krakenDB_viral/' -r2 '/home/ae42909/Scratch/smallTest_data/100_Potato_withViruses_2.fastq' -f 'fastq' -t 137758
