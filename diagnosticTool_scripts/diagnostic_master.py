115545b5fc3509d755386fe7ca29867adbe7dd19
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-12-07 11:50:46 +0000

    Added argparse for commandline variable calling

--- diagnosticTool_scripts/diagnostic_master.py
+++ diagnosticTool_scripts/diagnostic_master.py
@@ -3,120 +3,131 @@ import time
 import sys
 sys.path.insert(0, '/home/ae42909/viral_diagnostics/diagnosticTool_scripts/')
 from diagnostic_modules import *
+import argparse
 
-# General parameters
-file1 = "/home/ae42909/synthetic_RNAseq/mappingRNAseq/concatenated_fastaFiles/Potato_withViruses_1.fastq"
-file2 = "/home/ae42909/synthetic_RNAseq/mappingRNAseq/concatenated_fastaFiles/Potato_withViruses_2.fastq"
-user_format = "fastq"
-out_dir = "/home/ae42909/Scratch/synthPotato_pipeline/"
-threads = 4
-ncbi_file = '/home/ae42909/Scratch/kraken/kraken_analysis/customDatabase/NCBI_taxonomy.csv'
-subset = False
-
-# Trimmomatic paramters
-trim_minlen = 50
-adapter_file = "/mnt/apps/trimmomatic/IlluminaAdapters.fasta"
-
-# Kraken parameters
-kraken_db = "/home/ae42909/Scratch/krakenDB_k31_m15/"
-quick_minhits = False
-preload = False
-
-# Kaiju parameters
-kaiju_db = "/home/ae42909/Scratch/kaijuDB_viral_2/"
-kaiju_minlen = 15
-kaiju_score = 85
-kaiju_mismatch = 1
-
-# Write a log_file:
-with open(out_dir + "log_file.txt", "w")as log_file:
-    log_file.write("General parameters:\n" + "file1 = " + file1 + "\n" +
-                   "file2 = " + str(file2) + "\n" + 
-                   "output directory = " +  out_dir + "\n" +
-                   "threads =" + str(threads) + "\n" +
-                   "subset =" + str(subset) + "\n" +
-                   "Trimmomatic parameters:\n" + "trim_minlen = " + str(trim_minlen) + "\n" +
-                   "Kraken parameters:\n" + "kraken database = " + kraken_db + "\n" +
-                   "quick_minhits = " + str(quick_minhits) + "\n" + "preload =" +
-                   str(preload) + "\n" +
-                   "Kaiju parameters:\n" + "kaiju_db =" + kaiju_db + "\n" +
-                   "kaiju_minlen = " + str(kaiju_minlen) + "\n" +
-                   "kaiju_score = " + str(kaiju_score) + "\n" +
-                   "kaiju_mismatch = " + str(kaiju_mismatch) + "\n")
+parser = argparse.ArgumentParser(description='Kodoja')
+parser.add_argument('-r1', '--read1', type=str, required=True,
+                    help='Read 1 file path, required')
+parser.add_argument('-o', '--output_dir', type=str, required=True,
+                    help='Output directory path, required')
+parser.add_argument('-d1', '--kraken_db', type=str, required=True,
+                    help='Kraken database path, required')
+parser.add_argument('-d2', '--kaiju_db', type=str, required=True,
+                    help='Kaiju database path, required')
+parser.add_argument('-r2', '--read2', type=str, default=False,
+                    help='Read 2 file path')
+parser.add_argument('-f', '--data_format', type=str, default='fastq',
+                    help='Sequence data format')
+parser.add_argument('-t', '--threads', type=int, default=1,
+                    help='Number of threads')
+parser.add_argument('-s', '--subset', action='store_true',
+                    help='Subset data for Kaiju')
+parser.add_argument('-m', '--trim_minlen', type=int, default=50,
+                    help='Trimmomatic minimum length')
+parser.add_argument('-a', '--trim_adapt', type=str, default=False,
+                    help='Illumina adapter sequence file')
+parser.add_argument('-q', '--kraken_quick', type=int, default=False,
+                    help='Number of minium hits by Kraken')
+parser.add_argument('-p', '--kraken_preload', action='store_true',
+                    help='Kraken preload database')
+parser.add_argument('-c', '--kaiju_score', type=int, default=85,
+                    help='Kaju alignment score')
+parser.add_argument('-l', '--kaiju_minlen', type=int, default=15,
+                    help='Kaju minimum length')
+parser.add_argument('-i', '--kaiju_mismatch', type=int, default=85,
+                    help='Kaju allowed mismatches')
+args = parser.parse_args()
 
 # Check that dirs have "/" at the end
-out_dir += check_path(out_dir)
-kraken_db += check_path(kraken_db)
-kaiju_db += check_path(kaiju_db)
+args.output_dir += check_path(args.output_dir)
+args.kraken_db += check_path(args.kraken_db)
+args.kaiju_db += check_path(args.kaiju_db)
 
-# Check out_dir exits else make dir
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
+# Check args.output_dir exits else make dir
+if not os.path.exists(args.output_dir):
+    os.makedirs(args.output_dir)
 
-os.chdir(out_dir)
+os.chdir(args.output_dir)
+
+# Write a log_file:
+with open(args.output_dir + "log_file.txt", "w")as log_file:
+    log_file.write("General parameters:\n" + "file1 = " + args.read1 + "\n" +
+                   "file2 = " + str(args.read2) + "\n" +
+                   "output directory = " +  args.output_dir + "\n" +
+                   "threads =" + str(args.threads) + "\n" +
+                   "subset =" + str(args.subset) + "\n" +
+                   "Trimmomatic parameters:\n" + "trim_minlen = " + str(args.trim_minlen) + "\n" +
+                   "Kraken parameters:\n" + "kraken database = " + args.kraken_db + "\n" +
+                   "quick_minhits = " + str(args.kraken_quick) + "\n" + "preload =" +
+                   str(args.kraken_preload) + "\n" +
+                   "Kaiju parameters:\n" + "args.kaiju_db =" + args.kaiju_db + "\n" +
+                   "kaiju_minlen = " + str(args.kaiju_minlen) + "\n" +
+                   "kaiju_score = " + str(args.kaiju_score) + "\n" +
+                   "kaiju_mismatch = " + str(args.kaiju_mismatch) + "\n")
+
 
 t0 = time.time()
 # Test format, change seqIDs and check paired files are correct
-test_format(file1, user_format)
-check_file(file1, out_dir, user_format, file2)
+test_format(args.read1, args.data_format)
+check_file(args.read1, args.output_dir, args.data_format, args.read2)
 t1 = time.time()
 
 # Set all variables
-initial_file1 = 'renamed_file_1.' + user_format
+initial_file1 = 'renamed_file_1.' + args.data_format
 kraken_file1 = kaiju_file1 = "trimmed_read1"
 
-if file2:
+if args.read2:
     # Set tool files
     kraken_file2 = kaiju_file2 = "trimmed_read2"
-    initial_file2 = 'renamed_file_2.' + user_format
+    initial_file2 = 'renamed_file_2.' + args.data_format
 
 else:
     kraken_file2 = kaiju_file2 = False
     initial_file2 = False
 
-if user_format == "fastq":
+if args.data_format == "fastq":
     # fasta files cannot be QC'd - only for fastq files
-    fastqc_trim(out_dir, initial_file1, trim_minlen, threads, adapter_file, initial_file2)
+    fastqc_trim(args.output_dir, initial_file1, args.trim_minlen, args.threads, args.trim_adapt, initial_file2)
 else:
     kraken_file1 = kaiju_file1 = initial_file1
     kraken_file2 = kaiju_file2 = initial_file2
 
 
-if subset:
-    kaiju_file1 = "subset_file1." + user_format
+if args.subset:
+    kaiju_file1 = "subset_file1." + args.data_format
     if file2:
-        kaiju_file2 = "subset_file2." + user_format
+        kaiju_file2 = "subset_file2." + args.data_format
 t2 = time.time()
 
 # Kraken classification
-kraken_classify(kraken_file1, threads, user_format, kraken_db, kraken_file2,
-                quick_minhits = quick_minhits, preload = preload)
+kraken_classify(kraken_file1, args.threads, args.data_format, args.kraken_db, kraken_file2,
+                quick_minhits = args.kraken_quick, preload = args.kraken_preload)
 t3 = time.time()
 
 # Format kraken data and subset viral and unclassified sequences
-seq_reanalysis("kraken_table.txt", "kraken_labels.txt", ncbi_file, out_dir,
-               user_format, kraken_file1, subset, kraken_file2)
+seq_reanalysis("kraken_table.txt", "kraken_labels.txt", ncbi_file, args.output_dir,
+               args.data_format, kraken_file1, args.subset, kraken_file2)
 t4 = time.time()
 
 # Kaiju classification of all sequences or subset sequences
-kaiju_classify(kaiju_file1, threads, out_dir, kaiju_db, kaiju_minlen, kraken_db,
-               kaiju_file2, kaiju_mismatch = kaiju_mismatch,
-               kaiju_score = kaiju_score)
+kaiju_classify(kaiju_file1, args.threads, args.output_dir, args.kaiju_db, args.kaiju_minlen, args.kraken_db,
+               kaiju_file2, kaiju_mismatch = args.kaiju_mismatch,
+               kaiju_score = args.kaiju_score)
 t5 = time.time()     
 
 # Merege results
-result_analysis(out_dir, "kraken_VRL.txt", "kaiju_table.txt", "kaiju_labels.txt",
+result_analysis(args.output_dir, "kraken_VRL.txt", "kaiju_table.txt", "kaiju_labels.txt",
                 ncbi_file)
 t6 = time.time()
 
 
 # Create log file
-if subset:
+if args.subset:
     print_statment = "subset sequences = " + str((t4-t3)/60) + " min\n"
 else:
     print_statment = "formatting kraken data = " + str((t4-t3)/60) + " min\n"
 
-with open(out_dir + "log_file.txt", "a") as log_file:
+with open(args.output_dir + "log_file.txt", "a") as log_file:
     log_file.write("Script timer:\n" + "testing format/replace seqID = " + str((t1-t0)) + " s\n" +
                    "fastq and trim = " + str((t2-t1)/60) + " min\n" +
                    "kraken classification = " + str((t3-t2)/3600) + " h\n" +
