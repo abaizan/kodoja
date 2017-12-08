5fd864a6148ea44d31298e83cce57e4c1fe8e197
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-12-08 05:20:20 +0000

    Using new plant only databases

--- diagnosticTool_scripts/diagnostic_master.sh
+++ diagnosticTool_scripts/diagnostic_master.sh
@@ -6,4 +6,4 @@
 
 export PATH=/home/ae42909/Programs/Kraken/kraken-1.0/builddir:/mnt/apps/jellyfish/1.1.11/bin:/home/ae42909/Programs/Kaiju/kaiju-v1.5.0-linux-x86_64-static/bin:/mnt/apps/trimmomatic/0.32:$PATH
 
-python /home/ae42909/viral_diagnostics/diagnosticTool_scripts/diagnostic_master.py -r1 /home/ae42909/Scratch/smallTest_data/100_Potato_withViruses_1.fastq -r2 /home/ae42909/Scratch/smallTest_data/100_Potato_withViruses_2.fastq -o /home/ae42909/Scratch/100Seq_PE/ -d1 /home/ae42909/Scratch/diagnostic_databases/krakenDB_k31_m15/ -d2 /home/ae42909/Scratch/diagnostic_databases/kaijuDB_viral_2/ -t 4
+python /home/ae42909/viral_diagnostics/diagnosticTool_scripts/diagnostic_master.py -r1 /home/ae42909/Scratch/smallTest_data/100_Potato_withViruses_1.fastq -r2 /home/ae42909/Scratch/smallTest_data/100_Potato_withViruses_2.fastq -o /home/ae42909/Scratch/100Seq_PE/ -d1 /home/ae42909/Scratch/diagnostic_databases/krakenDB_k31_plnVir/ -d2 /home/ae42909/Scratch/diagnostic_databases/kaijuDB_plnVir/ -t 4
