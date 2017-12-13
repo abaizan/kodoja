3c5dca85a4b32977c34f15fd8cf938361c85e74a
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-12-13 16:34:59 +0000

    Added script and data to test diagnostic_master using travis

--- test/test_script.py
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#$ -cwd
-#$ -j yes
-#$ -pe smp 1
-
-python diagnosticTool_scripts/diagnostic_master.py -r1 data/testData_1.fastq -r2 data/testData_2.fastq -o 
-diff data/example_virus_table.txt 
