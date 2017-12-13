ee6feee6132387be2330f86c10ccbfaa63c30251
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-12-13 16:27:40 +0000

    Changed parameters

--- diagnosticTool_scripts/diagnostic_master.sh
+++ diagnosticTool_scripts/diagnostic_master.sh
@@ -2,8 +2,7 @@
 
 #$ -cwd
 #$ -j yes
-#$ -pe smp 4
+#$ -pe smp 1
 
-export PATH=/home/ae42909/Programs/Kraken/kraken-1.0/builddir:/mnt/apps/jellyfish/1.1.11/bin:/home/ae42909/Programs/Kaiju/kaiju-v1.5.0-linux-x86_64-static/bin:/mnt/apps/trimmomatic/0.32:$PATH
 
-python /home/ae42909/viral_diagnostics/diagnosticTool_scripts/diagnostic_master.py -r1 /home/ae42909/Scratch/smallTest_data/100_Potato_withViruses_1.fastq -r2 /home/ae42909/Scratch/smallTest_data/100_Potato_withViruses_2.fastq -o /home/ae42909/Scratch/100Seq_PE/ -d1 /home/ae42909/Scratch/diagnostic_databases/krakenDB_k31_plnVir/ -d2 /home/ae42909/Scratch/diagnostic_databases/kaijuDB_plnVir/ -t 4
+/home/ae42909/viral_diagnostics/diagnosticTool_scripts/diagnostic_master.py -r1 /home/ae42909/Scratch/smallTest_data/100_Potato_withViruses_1.fastq -o /home/ae42909/Scratch/100Seq_PE/ -d1 /home/ae42909/Scratch/kodoja_db/krakenDB_test/ -d2 /home/ae42909/Scratch/kodoja_db/kaijuDB_test/ -r2 /home/ae42909/Scratch/smallTest_data/100_Potato_withViruses_2.fastq
