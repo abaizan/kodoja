54fd472ad25c24f56b839d1c6708511b2fdd221c
Amanda Edge <amanda.edge@hutton.ac.uk>
2017-12-18 15:21:21 +0000

    Pre-populate kraken taxonomy for building test DB

--- diagnosticTool_scripts/database_master.sh
+++ diagnosticTool_scripts/database_master.sh
@@ -4,7 +4,15 @@
 #$ -j yes
 #$ -pe smp 10
 
+
+
 # Download test viruses and add them to mini db
+
+# Pre-create output folder with symlink to already downloaded taxonomy
+# (to avoid kraken downloading it all again)
+rm -rf /home/ae42909/Scratch/kodoja_db/
+mkdir -p /home/ae42909/Scratch/kodoja_db/krakenDB_test
+ln -s /home/ae42909/viral_diagnostics/test/taxonomy /home/ae42909/Scratch/kodoja_db/krakenDB_test/taxonomy
 /home/ae42909/viral_diagnostics/diagnosticTool_scripts/database_master.py --output_dir /home/ae42909/Scratch/kodoja_db/ --threads 10 --db_tag 'test' --test
 
 # Download all viruses  and add to db plant viruses
