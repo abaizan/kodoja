5819a69e48430c579f61246d8f32282e044fc081
Amanda Edge <amanda.edge@hutton.ac.uk>
2018-01-15 10:51:38 +0000

    TravisCI: Using 2nd paired end file in tests

--- test/test_script.sh
+++ test/test_script.sh
@@ -17,16 +17,17 @@ diff test/example_db/krakenDB_test/database.idx test/building_db/krakenDB_test/d
 # TODO: Compare more of the output files?
 
 # Run diagnostic tool
-diagnosticTool_scripts/diagnostic_master.py -r1 test/data/testData_1.fastq -o test/PE_test/ -d1 test/example_db/krakenDB_test/ -d2 test/example_db/kaijuDB_test/ -r2 ./test/data/testData_1.fastq -t 2
+# Paired end FASTQ
+diagnosticTool_scripts/diagnostic_master.py -r1 test/data/testData_1.fastq -o test/PE_test/ -d1 test/example_db/krakenDB_test/ -d2 test/example_db/kaijuDB_test/ -r2 ./test/data/testData_2.fastq -t 2
 diff test/PE_test/virus_table.txt test/data/example_virus_table.txt
 rm -r test/PE_test/
 
+# Single end FASTQ
 diagnosticTool_scripts/diagnostic_master.py -r1 test/data/testData_1.fastq -o test/SE_test/ -d1 test/example_db/krakenDB_test/ -d2 test/example_db/kaijuDB_test/ -t 2
 diff test/SE_test/virus_table.txt test/data/example_virus_table.txt
 rm -r test/SE_test/
 
-diagnosticTool_scripts/diagnostic_master.py -r1 test/data/testData_1.fasta -o test/fasta_test/ -d1 test/example_db/krakenDB_test/ -d2 test/example_db/kaijuDB_test/ -r2 ./test/data/testData_1.fasta -f fasta
+# Paired end FASTA
+diagnosticTool_scripts/diagnostic_master.py -r1 test/data/testData_1.fasta -o test/fasta_test/ -d1 test/example_db/krakenDB_test/ -d2 test/example_db/kaijuDB_test/ -r2 ./test/data/testData_2.fasta -f fasta
 diff test/fasta_test/virus_table.txt test/data/fastaEx_virus_table.txt
 rm -r test/fasta_test/
-
-
