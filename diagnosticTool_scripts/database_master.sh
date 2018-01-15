36d1e116a06befa41aeb5f71f51d299dc172333d
Amanda Edge <amanda.edge@hutton.ac.uk>
2018-01-15 10:01:38 +0000

    Change parameters

--- diagnosticTool_scripts/database_master.sh
+++ diagnosticTool_scripts/database_master.sh
@@ -7,10 +7,12 @@
 
 
 # Download test viruses and add them to mini db
-/home/ae42909/viral_diagnostics/diagnosticTool_scripts/database_master.py --output_dir /home/ae42909/Scratch/kodoja_db/ --threads 10 --db_tag 'test' --test --kraken_tax /home/ae42909/Scratch/kraken_taxonomy/
+# /home/ae42909/viral_diagnostics/diagnosticTool_scripts/database_master.py --output_dir /home/ae42909/Scratch/kodoja_db/ --threads 10 --db_tag 'test' --test --kraken_tax /home/ae42909/Scratch/kraken_taxonomy/
 
-# Download all viruses  and add to db plant viruses
+# Download all virus genomes but only add plant viruses to databases (no host)
 # /home/ae42909/viral_diagnostics/diagnosticTool_scripts/database_master.py --output_dir /home/ae42909/Scratch/kodoja_db/ --threads 10 --db_tag 'plnVir'
 
-# All vir genomes downloaded already in out_dir, add only plant viruses
+# All vir genomes downloaded already in out_dir, add only plant viruses (no host)
 # /home/ae42909/viral_diagnostics/diagnosticTool_scripts/database_master.py --output_dir /home/ae42909/Scratch/kodoja_db/ --threads 10 --db_tag 'allVir' --no_download --all_viruses
+
+
--- diagnosticTool_scripts/diagnostic_master.sh
+++ diagnosticTool_scripts/diagnostic_master.sh
@@ -2,7 +2,7 @@
 
 #$ -cwd
 #$ -j yes
-#$ -pe smp 10
+#$ -pe smp 4
 
 
-/home/ae42909/viral_diagnostics/diagnosticTool_scripts/diagnostic_master.py -r1 /home/ae42909/data_forTesting/Apple_data/AppleVirus_SRR1089477.fastq -o /home/ae42909/Scratch/100Seq_SE/ -d1 /home/ae42909/Scratch/diagnostic_databases/krakenDB_k31_plnVir/ -d2 /home/ae42909/Scratch/diagnostic_databases/kaijuDB_plnVir/ -m 30 -t 10
+/home/ae42909/viral_diagnostics/diagnosticTool_scripts/diagnostic_master.py -r1 /home/ae42909/data_forTesting/Algae_data/AlgaeVirus60_SRR924352.fastq -o /home/ae42909/Scratch/Algae_60/ -d1 /home/ae42909/Scratch/kodoja_db/krakenDB_algae_554065/ -d2 /home/ae42909/Scratch/kodoja_db/kaijuDB_algae_554065/ -m 30 -t 4
