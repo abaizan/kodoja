56c6bd5f2a9c1e516ca626b2f08cfbac3a8829c8
peterjc <p.j.a.cock@googlemail.com>
2018-01-16 12:02:02 +0000

    Move helper-functions out of check_file function.
    This nesting of the function definitions was a little confusing,
    and moving them also solves a flake8 docstrings warning:
    
    D202 No blank lines allowed after function docstring

--- diagnosticTool_scripts/diagnostic_modules.py
+++ diagnosticTool_scripts/diagnostic_modules.py
@@ -52,6 +52,51 @@ def test_format(file1, user_format):
         " format rather than " + user_format + " format."
 
 
+def str_overlap(str1, str2):
+    """Get number of matching characters between two strings.
+
+    Returns int with number of matching characters.
+    """
+    count = 0
+    for i in range(min(len(str1), len(str2))):
+        if str1[i] == str2[i]:
+            count += 1
+    return count
+
+
+def rename_seqIDs(input_file, out_dir, user_format, paired=False):
+    """Rename sequence ids.
+
+    Write a new file where each sequence ID is replaced with
+    the the first character (">" or "@") followed by a number
+    (1::N), and if it's a paired read followd by "/1" or "/2"
+    (called 'renamed_file'), and a dictionary composed of
+    dict[newID] = oldID.
+    """
+    output_file = out_dir + "renamed_file_"
+    if paired == 2:
+        output_file += str(paired) + "." + user_format
+    else:
+        output_file += "1." + user_format
+    id_dict = {}
+    with open(input_file, 'r') as in_file, open(output_file, 'w') as out_file:
+        seqNum = 1
+        seqid_line = (4 if user_format == 'fastq' else 2)
+
+        for lineNum, line in enumerate(in_file):
+            if lineNum % seqid_line == 0:
+                new_line = line[0] + str(seqNum)
+                id_dict[seqNum] = line[1:].strip()
+                if paired:
+                    new_line += '/' + str(paired)
+                new_line += "\n"
+                out_file.write(new_line)
+                seqNum += 1
+            else:
+                out_file.write(line)
+    return id_dict
+
+
 def check_file(file1, out_dir, user_format, file2=False):
     """Rename sequnce ids and check PE files.
 
@@ -63,54 +108,8 @@ def check_file(file1, out_dir, user_format, file2=False):
     the paired reads are mached by choosing a random
     entry from the first list of ids (minus meatdata) and
     the same entry for the second list of ids (can have
-    one character different  as could be named 1 or 2).
+    one character different as could be named 1 or 2).
     """
-
-    def str_overlap(str1, str2):
-        """Get number of matching characters between two strings.
-
-        Returns int with number of matching characters
-        """
-
-        count = 0
-        for i in range(min(len(str1), len(str2))):
-            if str1[i] == str2[i]:
-                count = count + 1
-        return count
-
-    def rename_seqIDs(input_file, out_dir, user_format, paired=False):
-        """Rename sequence ids.
-
-        Write a new file where each sequence ID is replaced with
-        the the first character (">" or "@") followed by a number
-        (1::N), and if it's a paired read followd by "/1" or "/2"
-        (called 'renamed_file'), and a dictionary composed of
-        dict[newID] = oldID.
-
-        """
-        output_file = out_dir + "renamed_file_"
-        if paired == 2:
-            output_file += str(paired) + "." + user_format
-        else:
-            output_file += "1." + user_format
-        id_dict = {}
-        with open(input_file, 'r') as in_file, open(output_file, 'w') as out_file:
-            seqNum = 1
-            seqid_line = (4 if user_format == 'fastq' else 2)
-
-            for lineNum, line in enumerate(in_file):
-                if lineNum % seqid_line == 0:
-                    new_line = line[0] + str(seqNum)
-                    id_dict[seqNum] = line[1:].strip()
-                    if paired:
-                        new_line += '/' + str(paired)
-                    new_line += "\n"
-                    out_file.write(new_line)
-                    seqNum += 1
-                else:
-                    out_file.write(line)
-        return id_dict
-
     if file2:
         ids1 = rename_seqIDs(file1, out_dir, user_format, paired=1)
         ids2 = rename_seqIDs(file2, out_dir, user_format, paired=2)
