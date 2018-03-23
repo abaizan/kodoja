abaizan/kodoja

commit 6de464c71af5f70247767721695d8e050aec695d

Use os.makedirs(...) and shutil.copyfile(...) rather than shell

diagnosticTool_scripts/kodoja_build.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import os
 import pandas as pd
 import argparse
-import subprocess
+import shutil
 
 from diagnostic_modules import version
 from diagnostic_modules import check_path
@@ -81,9 +81,9 @@ if args.extra_files:
             "File extensions need to be either '.fna' for genomic data" + \
             " or '.faa' for protein data, and be compressed ('.gz')"
     # Make a copy of each file in extra_files into 'extra' directory
-    subprocess.check_call("mkdir %sextra/" % args.output_dir, shell=True)
+    os.makedirs(args.output_dir + "extra/")
     for extraFile in args.extra_files:
-        subprocess.check_call("cp %s %s" % (extraFile, args.output_dir + 'extra/'), shell=True)
+        shutil.copyfile(extraFile, args.output_dir + 'extra/')
 
 # Download virus assembly summary for refseq
 if not os.path.exists(args.output_dir + "viral_assembly_summary.txt"):

commit 1c588130145bf70c12e386a4af58f4be66f901b5

Fix file copying extra files to a directory.
This fixes a bug I introduced in #11 when replacing
subprocess calls for file operations.

Currently the test suite does not use the extra files
arguments in kodoja_build.py

diagnosticTool_scripts/kodoja_build.py
@@ -85,7 +85,7 @@ if args.extra_files:
     # Make a copy of each file in extra_files into 'extra' directory
     os.makedirs(args.output_dir + "extra/")
     for extraFile in args.extra_files:
-        shutil.copyfile(extraFile, args.output_dir + 'extra/')
+        shutil.copy(extraFile, args.output_dir + 'extra/')
 
 # Download virus assembly summary for refseq
 if not os.path.exists(args.output_dir + "viral_assembly_summary.txt"):