aecef89be5a7c6a30c273c84617dedbb6240700d
peterjc <p.j.a.cock@googlemail.com>
2018-08-30 16:19:40 +0100

    Simplify code for filename of temporary read files

--- diagnosticTool_scripts/diagnostic_modules.py
+++ diagnosticTool_scripts/diagnostic_modules.py
@@ -79,11 +79,10 @@ def rename_seqIDs(input_file, out_dir, user_format, paired=False):
     Returns dictionary mapping the sequence number to the old
     identifier and description line.
     """
-    output_file = os.path.join(out_dir, "renamed_file_")
-    if paired == 2:
-        output_file += str(paired) + "." + user_format
+    if paired:
+        output_file = os.path.join(out_dir, "renamed_file_2." + user_format)
     else:
-        output_file += "1." + user_format
+        output_file = os.path.join(out_dir, "renamed_file_1." + user_format)
     id_dict = {}
     with open(input_file, 'r') as in_file, open(output_file, 'w') as out_file:
         if user_format == 'fasta':
